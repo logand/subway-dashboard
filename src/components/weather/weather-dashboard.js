0d065f4e7c063def0ebfcc80c6e3006d4bed0e71
Colin Brown <colindonovanbrown@gmail.com>
2018-06-13 16:32:40 -0400

    Fix multiple simultaneous updating with location changes

--- src/components/weather/weather-dashboard.js
+++ src/components/weather/weather-dashboard.js
@@ -16,7 +16,8 @@ export default class WeatherDashboard extends Component {
 
   state = {
     weatherData: null,
-    weatherTimerId: null
+    weatherTimerId: null,
+    newestFetchId: 0
   };
 
   static contextTypes = {
@@ -46,7 +47,6 @@ export default class WeatherDashboard extends Component {
         hasLocation &&
         (oldLatitude !== latitude || oldLongitude !== longitude)
       ) {
-        this.clearWeatherTimer();
         this.updateWeather(latitude, longitude);
       }
     }
@@ -57,18 +57,24 @@ export default class WeatherDashboard extends Component {
   }
 
   updateWeather(latitude, longitude) {
+    this.clearWeatherTimer();
+    const fetchId = this.state.newestFetchId + 1;
+    this.setState({newestFetchId: fetchId});
     this.fetchWeather(latitude, longitude).then(weatherData => {
-      const weatherTimerId = setTimeout(this.updateWeather.bind(this), 600000, latitude, longitude);
-      this.setState({
-        weatherData: weatherData,
-        weatherTimerId: weatherTimerId
-      });
+      if (fetchId === this.state.newestFetchId) {
+        const weatherTimerId = setTimeout(this.updateWeather.bind(this), 600000, latitude, longitude);
+        this.setState({
+          weatherData: weatherData,
+          weatherTimerId: weatherTimerId
+        });
+      };
     });
   }
-
+  
   clearWeatherTimer() {
     if (this.state.weatherTimerId) {
       clearTimeout(this.state.weatherTimerId);
+      this.setState({weatherTimerId: null});
     }
   }
 
